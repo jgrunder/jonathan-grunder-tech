90dea367645f7ae3b294c7720582f9809af88bc6
jgrunder <contact@jonathan-grunder.tech>
2021-01-21 14:54:19 +0100

    Transform experience scss

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -276,177 +276,152 @@ li > a::after {
     &:after {
       clear: both;
     }
+    & > .timeline-panel {
+      width: 95%;
+      float: left;
+      border: 1px solid #d4d4d4;
+      /*border-radius: 2px;*/
+      /*padding: 20px;*/
+      position: relative;
+      -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+      box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+      &:before {
+        position: absolute;
+        top: 26px;
+        right: -15px;
+        display: inline-block;
+        border-top: 15px solid transparent;
+        border-left: 15px solid #ccc;
+        border-right: 0 solid #ccc;
+        border-bottom: 15px solid transparent;
+        content: " ";
+      }
+      &:after {
+        position: absolute;
+        top: 27px;
+        right: -14px;
+        display: inline-block;
+        border-top: 14px solid transparent;
+        border-left: 14px solid #fff;
+        border-right: 0 solid #fff;
+        border-bottom: 14px solid transparent;
+        content: " ";
+      }
+    }
+    & > .timeline-badge {
+      color: #fff;
+      width: 24px;
+      height: 24px;
+      line-height: 50px;
+      font-size: 1.4em;
+      text-align: center;
+      position: absolute;
+      top: 16px;
+      right: -12px;
+      z-index: 100;
+    }
+    &.timeline-inverted > .timeline-panel {
+      float: right;
+      &:before {
+        border-left-width: 0;
+        border-right-width: 15px;
+        left: -15px;
+        right: auto;
+      }
+      &:after {
+        border-left-width: 0;
+        border-right-width: 14px;
+        left: -14px;
+        right: auto;
+      }
+    }
+    &.timeline-inverted {
+      float: right;
+      clear: right;
+      margin-top: 30px;
+      margin-bottom: 30px;
+      & > .timeline-badge {
+        left: -12px;
+      }
+    }
+    &:nth-child(2) {
+      margin-top: 60px;
+    }
   }
-}
-
-.timeline > li:before,
-.timeline > li:after {
-  content: " ";
-  display: table;
-}
-.timeline > li:after {
-  clear: both;
-}
-.timeline > li > .timeline-panel {
-  width: 95%;
-  float: left;
-  border: 1px solid #d4d4d4;
-  /*border-radius: 2px;*/
-  /*padding: 20px;*/
-  position: relative;
-  -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
-  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
-}
-
-.timeline > li > .timeline-panel:before {
-  position: absolute;
-  top: 26px;
-  right: -15px;
-  display: inline-block;
-  border-top: 15px solid transparent;
-  border-left: 15px solid #ccc;
-  border-right: 0 solid #ccc;
-  border-bottom: 15px solid transparent;
-  content: " ";
-}
-.timeline > li > .timeline-panel:after {
-  position: absolute;
-  top: 27px;
-  right: -14px;
-  display: inline-block;
-  border-top: 14px solid transparent;
-  border-left: 14px solid #fff;
-  border-right: 0 solid #fff;
-  border-bottom: 14px solid transparent;
-  content: " ";
-}
-.timeline > li > .timeline-badge {
-  color: #fff;
-  width: 24px;
-  height: 24px;
-  line-height: 50px;
-  font-size: 1.4em;
-  text-align: center;
-  position: absolute;
-  top: 16px;
-  right: -12px;
-  /*background-color: #999999;*/
-  z-index: 100;
-  /*
-  border-top-right-radius: 50%;
-  border-top-left-radius: 50%;
-  border-bottom-right-radius: 50%;
-  border-bottom-left-radius: 50%;
-  */
-}
-.timeline > li.timeline-inverted > .timeline-panel {
-  float: right;
-}
-.timeline > li.timeline-inverted > .timeline-panel:before {
-  border-left-width: 0;
-  border-right-width: 15px;
-  left: -15px;
-  right: auto;
-}
-.timeline > li.timeline-inverted > .timeline-panel:after {
-  border-left-width: 0;
-  border-right-width: 14px;
-  left: -14px;
-  right: auto;
-}
-.timeline-badge > a {
-  color: #C5C7C5 !important;
-}
-.timeline-badge a:hover {
-  color: #000 !important;
-}
-.timeline-title {
-  margin-top: 0;
-  color: inherit;
-}
-.timeline-body > p,
-.timeline-body > ul {
+  &-badge > a {
+    color: #C5C7C5 !important;
+  }
+  &-badge a:hover {
+    color: #000 !important;
+  }
+  &-title {
+    margin-top: 0;
+    color: inherit;
+  }
+  &-body > p, &-body > ul {
     padding:20px;
     margin-bottom: 0;
-}
-.timeline-body > p + p {
-  margin-top: 5px;
-}
-.timeline-footer{
+  }
+  &-body > p + p {
+    margin-top: 5px;
+  }
+&-footer {
     padding:20px;
     background-color:#f4f4f4;
-}
-.timeline-footer > a{
-    cursor: pointer;
-    text-decoration: none;
-}
-.timeline > li.timeline-inverted{
-  float: right;
-  clear: right;
-  margin-top: 30px;
-  margin-bottom: 30px;
-}
-.timeline > li:nth-child(2){
-  margin-top: 60px;
-}
-.timeline > li.timeline-inverted > .timeline-badge{
-  left: -12px;
+    & > a {
+      cursor: pointer;
+      text-decoration: none;
+    }
+  }
 }
 .clearfix {
   float: none !important;
   width: 10% !important;
 }
 @media (max-width: 767px) {
-    ul.timeline:before {
-        left: 40px;
+  ul.timeline {
+    &:before {
+      left: 40px;
     }
-
-    ul.timeline > li {
+    & > li {
       margin-bottom: 20px;
       position: relative;
       width:100%;
       float: left;
       clear: left;
-    }
-    ul.timeline > li > .timeline-panel {
+      & > .timeline-panel {
         width: calc(100% - 90px);
         width: -moz-calc(100% - 90px);
         width: -webkit-calc(100% - 90px);
-    }
-
-    ul.timeline > li > .timeline-badge {
+        float: right;
+        &:before {
+          border-left-width: 0;
+          border-right-width: 15px;
+          left: -15px;
+          right: auto;
+        }
+        &:after {
+          border-left-width: 0;
+          border-right-width: 14px;
+          left: -14px;
+          right: auto;
+        }
+      }
+      & > .timeline-badge {
         left: 28px;
         margin-left: 0;
         top: 16px;
-    }
-
-    ul.timeline > li > .timeline-panel {
-        float: right;
-    }
-
-        ul.timeline > li > .timeline-panel:before {
-            border-left-width: 0;
-            border-right-width: 15px;
-            left: -15px;
-            right: auto;
+      }
+      &.timeline-inverted {
+        float: left;
+        clear: left;
+        margin-top: 30px;
+        margin-bottom: 30px;
+        & > .timeline-badge{
+          left: 28px;
         }
-
-        ul.timeline > li > .timeline-panel:after {
-            border-left-width: 0;
-            border-right-width: 14px;
-            left: -14px;
-            right: auto;
-        }
-
-  .timeline > li.timeline-inverted{
-    float: left;
-    clear: left;
-    margin-top: 30px;
-    margin-bottom: 30px;
-  }
-
-  .timeline > li.timeline-inverted > .timeline-badge{
-    left: 28px;
+      }
+    }
   }
 }
 </style>
